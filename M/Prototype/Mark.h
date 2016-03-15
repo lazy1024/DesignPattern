f4031a2f21c04ee5a4eb48d2368e0cf20be4115a
李永亮 <lazy10240@126.com>
2016-03-15 22:11:05 +0800

    add mark

---
+++ M/Prototype/Mark.h
@@ -0,0 +1,14 @@
+//
+//  Mark.h
+//  DesignPattern
+//
+//  Created by 李永亮 on 16/3/15.
+//  Copyright © 2016年 李永亮. All rights reserved.
+//
+
+#ifndef Mark_h
+#define Mark_h
+
+// 111111
+
+#endif /* Mark_h */
