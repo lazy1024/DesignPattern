f2aa945c2416c7af247d646fc032f0e8804ac8e9
李永亮 <lazy10240@126.com>
2016-03-16 22:22:19 +0800

    Factory

---
+++ Factory/CanvasView.m
@@ -0,0 +1,21 @@
+//
+//  CanvasView.m
+//  DesignPattern
+//
+//  Created by 李永亮 on 16/3/16.
+//  Copyright © 2016年 李永亮. All rights reserved.
+//
+
+#import "CanvasView.h"
+
+@implementation CanvasView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    return nil;
+}
+- (id)addSubview:(UIView *)view
+{
+    return nil;
+}
+@end
